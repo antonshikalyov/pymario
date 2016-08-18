fe51063376e72e0499b508d6cd22c6f05da11c20
antonshikalyov <anton.shikalyov@gmail.com>
2016-08-18 22:48:46 +0300

    basic movement

--- main.py
+++ main.py
@@ -1,20 +1,35 @@
 import pygame
 pygame.init()
-screen = pygame.display.set_mode((800,600))
+width = 1500
+height = 1300
+screen = pygame.display.set_mode((height,width))
 speed = [2,2]
 black = 0,0,0
-width = 800
-height = 600
-gamer = pygame.image.load('mario.png')
-ballrect = gamer.get_rect()
+gamer = pygame.image.load('./images/mario.png')
+x = 10
+y = 10
 while 1:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT: sys.exit()
-	ballrect = ballrect.move(speed)
-	if ballrect.left < 0 or ballrect.right > width:
-		speed[0] = -speed[0]
-	if ballrect.top < 0 or ballrect.bottom > height:
-		speed[1] = -speed[1]
+		if event.type == pygame.KEYDOWN:
+			if event.key == 274:
+				print('up')
+				y = y+50
+			if event.key == 273:
+				print('down')
+				y = y-50
+			if event.key == 275:
+				print('right')
+				print(x)				
+				x = x+50
+				if x>500:
+					x=10
+			if event.key == 276:
+				print('left')
+				print(x)
+				x = x-50
+
+
 	screen.fill(black)
-	screen.blit(gamer, ballrect)
+	screen.blit(gamer, (x,y))
 	pygame.display.flip()
